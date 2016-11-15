9050c0f3c55040e67198ca46d50e3232401e62da
thisisfatih <trustme@igctw.org>
2016-11-15 14:10:27 +0300

    Add project files.

---
+++ authenticationBackend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(authenticationBackend.Startup))]
+
+namespace authenticationBackend
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureMobileApp(app);
+		}
+	}
+}
